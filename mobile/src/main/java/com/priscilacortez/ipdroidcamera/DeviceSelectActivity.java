c31712a44179f29ab1d22e486011fee0675daa18
Priscila Cortez <priscila.cortez@ibm.com>
2017-02-13 16:45:27 -0600

    updated opencv sdk gradle file

--- mobile/src/main/java/com/priscilacortez/ipdroidcamera/DeviceSelectActivity.java
+++ mobile/src/main/java/com/priscilacortez/ipdroidcamera/DeviceSelectActivity.java
@@ -3,7 +3,8 @@ package com.priscilacortez.ipdroidcamera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class DeviceSelectActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
