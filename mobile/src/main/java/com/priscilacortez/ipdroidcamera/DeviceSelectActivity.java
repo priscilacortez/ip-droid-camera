bc83b0cd082c660b6e20d5a0355fbd44161d63da
Priscila Cortez <priscila.cortez@ibm.com>
2017-02-14 13:34:36 -0600

    scan button and custom toolbar added

--- mobile/src/main/java/com/priscilacortez/ipdroidcamera/DeviceSelectActivity.java
+++ mobile/src/main/java/com/priscilacortez/ipdroidcamera/DeviceSelectActivity.java
@@ -2,6 +2,10 @@ package com.priscilacortez.ipdroidcamera;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.Window;
 
 public class DeviceSelectActivity extends AppCompatActivity {
 
@@ -9,6 +13,21 @@ public class DeviceSelectActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // get spinner in upper right corner
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_main);
+
+        // set custom toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Setup Bluetooth devices list with custom rows
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.bluetooth_scan_action,menu);
+        return true;
     }
 }
